bec3816467d80db7ad26c28be1d5e0e799230b3c
Sudharshan <72936645+sudharshan2001@users.noreply.github.com>
2022-09-14 21:29:58 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -28,3 +28,21 @@ def mlp(x, hidden_units, dropout_rate):
         patch_dims = patches.shape[-1]
         patches = tf.reshape(patches, [batch_size, -1, patch_dims])
         return patches
+
+ 
+
+class PatchEncoder(L.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = L.Dense(units = projection_dim
+                                  , activation=tf.nn.relu
+                                 )
+        self.position_embedding = L.Embedding(
+            input_dim = num_patches, output_dim = projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start = 0, limit = self.num_patches, delta = 1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
