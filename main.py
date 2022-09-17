05e469942baab496bc98aac7507e8be60cf7c398
Sudharshan <72936645+sudharshan2001@users.noreply.github.com>
2022-09-17 21:36:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,3 +50,54 @@ x_train=np.array(x_train)
 y_train=df_train['target']
 
 x_train,x_test,y_train,y_test=train_test_split(x_train,y_train,test_size=0.1)
+
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+                rotation_range=20,
+          
+                zoom_range=0.3,
+
+
+#                                                           preprocessing_function = data_augment
+)
+
+train_gen = train_datagen.flow_from_dataframe(dataframe = df_train,
+                                        x_col='Ids',
+                                        y_col='label',
+#                                         subset = 'training',
+                                        batch_size = batch_size,
+                                        seed = 1,
+                                        color_mode = 'rgb',
+                                        shuffle = True,
+                                        class_mode = 'categorical',
+                                        target_size = (image_size, image_size))
+
+valid_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+#                 rotation_range=30,
+#                 width_shift_range=0.5,
+#                 height_shift_range=0.5,
+#                 zoom_range=0.3,
+#                 samplewise_center = True,
+#                 samplewise_std_normalization = True,
+#                                                           preprocessing_function = data_augment
+)
+
+valid_gen = valid_datagen.flow_from_dataframe(dataframe = df_val,
+                                        x_col='Ids',
+                                        y_col='label',
+#                                         subset = 'validation',
+                                        batch_size = batch_size,
+                                        seed = 1,
+                                        color_mode = 'rgb',
+                                        shuffle = False,
+                                        class_mode = 'categorical',
+                                        target_size = (image_size, image_size))
+
+
+
+decay_steps = train_gen.n // train_gen.batch_size/ 
+
+initial_learning_rate = learning_rate
+
+lr_decayed_fn = tf.keras.experimental.CosineDecay(initial_learning_rate, decay_steps)
+
+lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lr_decayed_fn)
